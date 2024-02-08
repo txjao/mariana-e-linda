2c23844cebebc3793bda6d01ca382198bf04f8d3
João <jaobacanao@gmail.com>
2024-02-07 21:11:46 -0300

    core: added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import wazowski from '../assets/wazowski.png'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
